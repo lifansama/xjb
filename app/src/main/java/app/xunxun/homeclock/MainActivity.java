9d18e4953348bb21f13c3d65186e8d2efcf2a947
fengdianxun <731270777@qq.com>
2017-01-12 22:45:54 +0800

    修复最后一位为1时位置移动的bug

--- app/src/main/java/app/xunxun/homeclock/MainActivity.java
+++ app/src/main/java/app/xunxun/homeclock/MainActivity.java
@@ -1,7 +1,6 @@
 package app.xunxun.homeclock;
 
 import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -9,13 +8,12 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -30,8 +28,6 @@ import butterknife.InjectView;
 public class MainActivity extends AppCompatActivity {
 
     public static final int WHAT_TIME = 1;
-    @InjectView(R.id.timeTv)
-    TextView timeTv;
     @InjectView(R.id.dateTv)
     TextView dateTv;
     @InjectView(R.id.weekTv)
@@ -40,30 +36,39 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout activityMain;
     @InjectView(R.id.rootFl)
     FrameLayout rootFl;
+    @InjectView(R.id.hoursTv)
+    TextView hoursTv;
+    @InjectView(R.id.minusTv)
+    TextView minusTv;
+    @InjectView(R.id.secondTv)
+    TextView secondTv;
+    @InjectView(R.id.placeholderHoursTv)
+    TextView placeholderHoursTv;
+    @InjectView(R.id.placeholderMinusTv)
+    TextView placeholderMinusTv;
+    @InjectView(R.id.placeholderSecondTv)
+    TextView placeholderSecondTv;
 
     private Timer timer;
     private TimerTask timerTask;
     private Handler handler;
-    private SimpleDateFormat timeSDF = new SimpleDateFormat("HH:mm:ss");
     private SimpleDateFormat dateSDF = new SimpleDateFormat("yyyy-MM-dd");
     private SimpleDateFormat weekSDF = new SimpleDateFormat("E");
 
     private DoubleClickExit doubleClickExit;
-    private GestureDetectorCompat gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-//            Window window = getWindow();
-//            WindowManager.LayoutParams params = window.getAttributes();
-//            params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-//            window.setAttributes(params);
-//        }
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
         Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/ds_digi.ttf");
-        timeTv.setTypeface(typeFace);
+        hoursTv.setTypeface(typeFace);
+        minusTv.setTypeface(typeFace);
+        secondTv.setTypeface(typeFace);
+        placeholderHoursTv.setTypeface(typeFace);
+        placeholderMinusTv.setTypeface(typeFace);
+        placeholderSecondTv.setTypeface(typeFace);
         dateTv.setTypeface(typeFace);
 
         handler = new Handler() {
@@ -71,9 +76,16 @@ public class MainActivity extends AppCompatActivity {
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 if (msg.what == 1) {
+                    Calendar calendar = Calendar.getInstance();
+                    if (hoursTv != null && minusTv != null && secondTv != null){
+                        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                        int minute = calendar.get(Calendar.MINUTE);
+                        int second = calendar.get(Calendar.SECOND);
+                        hoursTv.setText(String.format("%02d",hours));
+                        minusTv.setText(String.format("%02d",minute));
+                        secondTv.setText(String.format("%02d",second));
+                    }
                     Date now = new Date();
-                    if (timeTv != null)
-                        timeTv.setText(timeSDF.format(now));
                     if (dateTv != null)
                         dateTv.setText(dateSDF.format(now));
                     if (weekTv != null)
@@ -110,7 +122,9 @@ public class MainActivity extends AppCompatActivity {
         };
         timer.schedule(timerTask, 1000, 1000);
         activityMain.setBackgroundColor(BackgroundColorPreferencesDao.get(this));
-        timeTv.setTextColor(TextColorPreferencesDao.get(this));
+        hoursTv.setTextColor(TextColorPreferencesDao.get(this));
+        minusTv.setTextColor(TextColorPreferencesDao.get(this));
+        secondTv.setTextColor(TextColorPreferencesDao.get(this));
         dateTv.setTextColor(TextColorPreferencesDao.get(this));
         weekTv.setTextColor(TextColorPreferencesDao.get(this));
     }
