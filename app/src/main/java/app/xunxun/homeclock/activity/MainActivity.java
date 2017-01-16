3dd0e123f62e0f2211624bfa964548d6508faff0
fengdianxun <731270777@qq.com>
2017-01-16 23:26:42 +0800

    换了一种新字体

--- app/src/main/java/app/xunxun/homeclock/activity/MainActivity.java
+++ app/src/main/java/app/xunxun/homeclock/activity/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
