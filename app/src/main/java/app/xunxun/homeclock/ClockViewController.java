f557b18f469961e49bc4d4d31ae7af649d30f293
fengdianxun <731270777@qq.com>
2017-01-19 17:19:10 +0800

    取消长按屏幕打开设置的方式

--- app/src/main/java/app/xunxun/homeclock/ClockViewController.java
+++ app/src/main/java/app/xunxun/homeclock/ClockViewController.java
@@ -100,14 +100,6 @@ public class ClockViewController {
                 }
             }
         };
-        rootFl.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Log.v("activityMain", "onLongClick");
-                trans2Settins();
-                return false;
-            }
-        });
         rootFl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
