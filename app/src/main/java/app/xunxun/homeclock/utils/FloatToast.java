40c07bc6d0fc4884b4e46d13eabb1d7d8ac675c4
fengdianxun <731270777@qq.com>
2017-05-02 22:47:35 +0800

    bugfix

--- app/src/main/java/app/xunxun/homeclock/utils/FloatToast.java
+++ app/src/main/java/app/xunxun/homeclock/utils/FloatToast.java
@@ -1,13 +1,14 @@
 package app.xunxun.homeclock.utils;
 
 import android.app.Activity;
-import android.content.Context;
+import android.os.Build;
 import android.os.Handler;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by fengdianxun on 2017/4/22.
@@ -21,23 +22,29 @@ public class FloatToast {
 
     public void show(final Activity activity, String text, View view) {
 
-        popupWindow = new PopupWindow(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        TextView textView = new TextView(activity);
-        textView.setTextSize(16);
-        textView.setGravity(Gravity.CENTER);
-        textView.setPadding(0,16,0,16);
-        textView.setText(text);
-        popupWindow.setContentView(textView);
-        popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
 
-                if (activity != null && !activity.isFinishing()&&popupWindow != null && popupWindow.isShowing()) {
-                    popupWindow.dismiss();
+            popupWindow = new PopupWindow(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            TextView textView = new TextView(activity);
+            textView.setTextSize(16);
+            textView.setGravity(Gravity.CENTER);
+            textView.setPadding(0, 16, 0, 16);
+            textView.setText(text);
+            popupWindow.setContentView(textView);
+            popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (activity != null && !activity.isFinishing() && popupWindow != null && popupWindow.isShowing()) {
+                        popupWindow.dismiss();
+                    }
                 }
-            }
-        }, 2000);
+            }, 2000);
+
+        } else {
+            Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
+        }
     }
 
 
