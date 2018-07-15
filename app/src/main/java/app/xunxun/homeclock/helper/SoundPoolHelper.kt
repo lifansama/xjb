90bf2e947e4e2e1beb3bfb0404273d37e0a56c86
fengdianxun <731270777@qq.com>
2018-07-15 21:41:25 +0800

    升级gradle

--- app/src/main/java/app/xunxun/homeclock/helper/SoundPoolHelper.kt
+++ app/src/main/java/app/xunxun/homeclock/helper/SoundPoolHelper.kt
@@ -1,2 +1,21 @@
 package app.xunxun.homeclock.helper
 
+import android.media.SoundPool
+
+/**
+ * 整点报时声音类.
+ */
+class SoundPoolHelper(val soundPool:SoundPool){
+
+    init {
+
+
+    }
+
+    fun load(){}
+
+    fun play(){}
+
+    fun release(){}
+
+}
