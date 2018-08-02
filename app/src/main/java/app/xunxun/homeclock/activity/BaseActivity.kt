7fd8cc3ede3dc47ca62997bd998d900108334430
fengdianxun <731270777@qq.com>
2018-08-02 22:48:45 +0800

    增加直接回home

--- app/src/main/java/app/xunxun/homeclock/activity/BaseActivity.kt
+++ app/src/main/java/app/xunxun/homeclock/activity/BaseActivity.kt
@@ -1,18 +1,19 @@
 package app.xunxun.homeclock.activity
 
-import android.app.Activity
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
 import android.view.WindowManager
-
 import app.xunxun.homeclock.MyApplication
+import app.xunxun.homeclock.R
 import app.xunxun.homeclock.preferences.KeepScreenOnPreferencesDao
 import com.umeng.analytics.MobclickAgent
+import org.jetbrains.anko.clearTop
+import org.jetbrains.anko.intentFor
 
 /**
  * Created by fengdianxun on 2017/4/22.
@@ -22,8 +23,8 @@ open class BaseActivity : AppCompatActivity() {
 
     private var dialog: AlertDialog? = null
     private var countDownTimer: CountDownTimer? = null
-    protected var disableCountDown:Boolean = false
-    private lateinit var initialTitle:CharSequence
+    protected var disableCountDown: Boolean = false
+    private lateinit var initialTitle: CharSequence
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,6 +66,7 @@ open class BaseActivity : AppCompatActivity() {
         MobclickAgent.onPause(this)
         countDownTimer?.cancel()
     }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         //        if (requestCode == 100){
         //            if (grantResults.length>0&&grantResults[0] == PackageManager.PERMISSION_DENIED) {
@@ -72,9 +74,17 @@ open class BaseActivity : AppCompatActivity() {
         //            }
         //        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
             onBackPressed()
+        } else if (item.itemId == R.id.action_home) {
+            startActivity(intentFor<MainActivity>().clearTop())
         }
         return super.onOptionsItemSelected(item)
     }
